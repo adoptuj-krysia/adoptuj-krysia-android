d14a6821d5f2a5a7b014412ca572745e06e22acb
Adoptuj Krysia <adoptuj-krysia@protonmail.com>
2020-11-19 10:28:37 +0100

    Krychotron now has extra sound when opened at 3:33 AM

--- app/src/main/java/pl/tuchola/zslit/krychu/krychotron/KrychotronEntryFactory.kt
+++ app/src/main/java/pl/tuchola/zslit/krychu/krychotron/KrychotronEntryFactory.kt
@@ -1,9 +1,14 @@
 package pl.tuchola.zslit.krychu.krychotron
 import pl.tuchola.zslit.krychu.R
+import java.util.Calendar
 
 class KrychotronEntryFactory {
     fun getEntries() : Array<KrychotronEntry> {
-        return arrayOf (
+        val cal = Calendar.getInstance()
+        val hour = cal.get(Calendar.HOUR_OF_DAY)
+        val minute = cal.get(Calendar.MINUTE)
+
+        val entries = mutableListOf(
             KrychotronEntry(R.raw.krychotron_0_1, "One są wyposażone w panel krosowy…"),
             KrychotronEntry(R.raw.krychotron_0_2, "Przełącznik sieciowy switch…"),
             KrychotronEntry(R.raw.krychotron_0_3, "Serwer…"),
@@ -12,5 +17,11 @@ class KrychotronEntryFactory {
             KrychotronEntry(R.raw.krychotron_0_6, "Router bezprzewodowy…"),
             KrychotronEntry(R.raw.krychotron_0_7, "To, co jest wymagane, na egzamin…")
         )
+
+        if(hour == 3 && minute in 30..35) {
+            entries.add(0, KrychotronEntry(R.raw.krychotron_extra_1, "Biały Kryś [3:33 special]"))
+        }
+
+        return entries.toTypedArray()
     }
 }
